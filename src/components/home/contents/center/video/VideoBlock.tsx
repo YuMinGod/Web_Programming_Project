517ff4349c10bc51d2606dd41408b2d35580fd0a
Jung Yu Min <jungym0419@gmail.com>
2023-11-21 13:59:44 +0900

    Update Video Link

--- src/components/home/contents/center/video/VideoBlock.tsx
+++ src/components/home/contents/center/video/VideoBlock.tsx
@@ -22,7 +22,7 @@ const VideoBlock:React.SFC = () => {
         <Wrapper>
             <BlockFrame col_title="Video" col_desc="해당 프로젝트 제작의 참고 영상입니다.">
                 <VideoWrapper>
-                    <iframe title="youtube" src="https://www.youtube.com/embed/52TT7SLexxE" frameBorder={0} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}/>
+                    <iframe title="youtube" src="https://www.youtube.com/embed/dtBNnijA1QU?si=me1ANu9dTXCyntyu" frameBorder={0} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}/>
                 </VideoWrapper>
             </BlockFrame>
         </Wrapper>
